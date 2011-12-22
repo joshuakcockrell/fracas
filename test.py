ffbb6cfa79dc7d5009bf9d9d9aa8e4d2bfae34b1
cabot696@gmail.com <cabot696@gmail.com@9e7094bd-7a5e-2906-86d2-6e0f91eff6ce>
2011-12-22 09:37:33 +0000

    Yay, it works! The painting still disappears if you switch away from the window. We have to draw to a Pixmap and display the pixmap for the image to stay. It'll probably render faster and look better too.

--- test.py
+++ test.py
@@ -1,43 +1,18 @@
-import sys
-from PySide import QtGui, QtCore
-
-class Example(QtGui.QWidget):
-    
+class Hi():
     def __init__(self):
-        super(Example, self).__init__()
-        print self.width
-        self.screen_width = 600
-        self.screen_height = 400
+        self.garbage = 10
 
-        self.initQuitButton()
-        self.initUI()
+    def get_some_garbage(self):
+        return self.garbage
 
-    def quit_program(self):
-        return QtCore.QCoreApplication.instance().quit
-
-    def initQuitButton(self):
-        quit_button = QtGui.QPushButton('Quit button', self)
-        
-        quit_button.clicked.connect(self.quit_program()) # quitting
-        
-        quit_shape = quit_button.sizeHint()
-        quit_size = [quit_shape.width(), quit_shape.height()]
-        quit_button.resize(quit_shape)
-        top = 0
-        left = self.screen_width - quit_size[0]
-        quit_button.move(left, top) # topleft
-
-    def initUI(self):               
-        self.setGeometry(10, 30, self.screen_width, self.screen_height) # size of window
-        self.setWindowTitle('Quit button')    
-        self.show()
-        
-def main():
-    
-    app = QtGui.QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+class Soup(Hi):
+    def __init__(self):
+        Hi.__init__(self)
 
 
-if __name__ == '__main__':
-    main()
+soup = Soup()
+
+print soup.__dict__.keys()
+for k in soup.__dict__.keys():
+    print str(k) + ' :' + str(soup.__dict__[k])
+print dir(soup)
